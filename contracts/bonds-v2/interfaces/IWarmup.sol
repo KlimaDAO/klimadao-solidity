2d00038c3efe01b19388f337dfd024dd0354d388
Archimedes <archimedescrypto@protonmail.com>
2022-05-06 17:01:58 -0600

    inital Ctrl+C , Ctrl+V of Olympus V2 bonds
    compiles bond depository, need to go and check the rest of the code to make sure no weird interactions occur

---
+++ contracts/bonds-v2/interfaces/IWarmup.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity >=0.7.5;
+
+interface IWarmup {
+    function retrieve(address staker_, uint256 amount_) external;
+}
--- contracts/interfaces/IwsKLIMA.sol
+++ contracts/interfaces/IwsKLIMA.sol
@@ -2,7 +2,9 @@
 
 pragma solidity ^0.8.0;
 
-interface IwsKLIMA {
+import "../bonds-v2/interfaces/IERC20.sol";
+
+interface IwsKLIMA is IERC20 {
     function wrap(uint256 _amount) external returns (uint256);
 
     function unwrap(uint256 _amount) external returns (uint256);
