950312bd479379094a4eb3cdf79657441250d3b4
ChazSchmidt <schmidt.864@osu.edu>
2024-01-30 20:55:56 -0500

    install foundry in vercel build script

--- build.js
+++ build.js
@@ -1,12 +1,28 @@
-// this build script is made to run forge build and forge doc to build the solidity contract doc html files and then vercel can deploy them
-
 const { exec } = require('child_process');
 
-exec('forge doc --build', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.error(`stderr: ${stderr}`);
+exec('curl -L https://foundry.paradigm.xyz | bash', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+
+    exec('foundryup', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+
+        exec('forge doc --build', (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+            console.error(`stderr: ${stderr}`);
+        });
+    });
 });
