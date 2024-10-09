88743627ed2b00e647c1fb882a6610762809b106
psparacino <tufnelenterprises@gmail.com>
2024-10-09 13:50:54 -0400

    updated swap paths to only sushiRouter

--- src/infinity/init/NativeUSDCInit.sol
+++ src/infinity/init/NativeUSDCInit.sol
@@ -28,17 +28,15 @@ contract NativeUSDCInit {
         s.swap[C.mco2()][C.usdc_bridged()].swapPaths[1] = [C.klima(), C.mco2()];
 
         /* Default UBO Swap setup */
-        s.swap[C.ubo()][C.usdc_bridged()].swapDexes = [0, 1];
-        s.swap[C.ubo()][C.usdc_bridged()].ammRouters = [C.sushiRouter(), C.sushiTridentRouter()];
-        s.swap[C.ubo()][C.usdc_bridged()].swapPaths[0] = [C.usdc_bridged(), C.klima()];
-        s.swap[C.ubo()][C.usdc_bridged()].swapPaths[1] = [C.klima(), C.ubo()];
+        s.swap[C.ubo()][C.usdc_bridged()].swapDexes = [0];
+        s.swap[C.ubo()][C.usdc_bridged()].ammRouters = [C.sushiRouter()];
+        s.swap[C.ubo()][C.usdc_bridged()].swapPaths[0] = [C.usdc_bridged(), C.klima(), C.ubo()];
 
         /* Default NBO Swap setup */
-        s.swap[C.nbo()][C.usdc_bridged()].swapDexes = [0, 1];
-        s.swap[C.nbo()][C.usdc_bridged()].ammRouters = [C.sushiRouter(), C.sushiTridentRouter()];
-        s.swap[C.nbo()][C.usdc_bridged()].swapPaths[0] = [C.usdc_bridged(), C.klima()];
-        s.swap[C.nbo()][C.usdc_bridged()].swapPaths[1] = [C.klima(), C.nbo()];
-
+        s.swap[C.nbo()][C.usdc_bridged()].swapDexes = [0];
+        s.swap[C.nbo()][C.usdc_bridged()].ammRouters = [C.sushiRouter()];
+        s.swap[C.nbo()][C.usdc_bridged()].swapPaths[0] = [C.usdc_bridged(), C.klima(), C.nbo()];
+        
         /* Default Coorest CCO2 Swap Setup */
         s.swap[C.coorestCCO2Token()][C.usdc_bridged()].swapDexes = [0];
         s.swap[C.coorestCCO2Token()][C.usdc_bridged()].ammRouters = [C.sushiRouter()];
