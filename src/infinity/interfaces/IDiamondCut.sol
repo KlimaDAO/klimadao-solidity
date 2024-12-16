a9bdc92caa530e00399d71b917878454a7c7cc5d
psparacino <tufnelenterprises@gmail.com>
2024-12-16 09:34:40 -0500

    fix author natspec

--- src/infinity/interfaces/IDiamondCut.sol
+++ src/infinity/interfaces/IDiamondCut.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 /**
  * \
- * Author: Nick Mudge <nick@perfectabstractions.com> (https://twitter.com/mudgen)
+ * @author Nick Mudge <nick@perfectabstractions.com> (https://twitter.com/mudgen)
  * EIP-2535 Diamonds: https://eips.ethereum.org/EIPS/eip-2535
  * /*****************************************************************************
  */
