16a0a89c730a06c19b12eaaafe0dbc03ed9b65e5
Marcus Aurelius <marcus_aurelius_0x@protonmail.com>
2025-02-06 18:09:39 -0500

    Add additional test for other facet

--- test/infinity/upgrades/4_upgradeInfinityForCMARK.t.sol
+++ test/infinity/upgrades/4_upgradeInfinityForCMARK.t.sol
@@ -10,17 +10,17 @@ import { Test } from "forge-std/Test.sol";
 import { IERC20 } from "../../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import { ICarbonmark } from "../../../src/infinity/interfaces/ICarbonmark.sol";
 import { ICMARKCreditTokenFactory } from "../../../src/infinity/interfaces/ICMARKCredit.sol";
-import { RetireInfoFacet } from "../../../src/infinity/facets/Retire/RetireInfoFacet.sol";
 import { LibRetire } from "../../../src/infinity/libraries/LibRetire.sol";
 import { LibTransfer } from "../../../src/infinity/libraries/Token/LibTransfer.sol";
+import { RetireCMARKFacet } from "../../../src/infinity/facets/Bridges/CMARK/RetireCMARKFacet.sol";
 import { RetireCarbonmarkFacet } from "../../../src/infinity/facets/Retire/RetireCarbonmarkFacet.sol";
 
 contract UpgradeInfinityForCMARKCredits is TestHelper, AssertionHelper, ListingsHelper {
     UpgradeInfinityForCMARK upgradeScript;
     address USDC_ADDRESS;
 
-    uint256 constant AMOUNT = 2 ether;
-    uint256 constant SOURCE_AMOUNT = 1 ether;
+    uint256 constant AMOUNT = 4 ether;
+    uint256 constant SOURCE_AMOUNT = 2 ether;
     uint256 constant unitPrice = 1 ether;
     uint256 constant minFillAmount = 1e18;
     address RETIRER = vm.addr(1);
@@ -111,7 +111,6 @@ contract UpgradeInfinityForCMARKCredits is TestHelper, AssertionHelper, Listings
 
         ICarbonmark marketplace = ICarbonmark(MARKETPLACE_ADDRESS);
         RetireCarbonmarkFacet diamond = RetireCarbonmarkFacet(DIAMOND_ADDRESS);
-        RetireInfoFacet rifacet = RetireInfoFacet(DIAMOND_ADDRESS);
 
         bytes32 cmarkListingId =
             marketplace.createListing(CMARK_ADDRESS, AMOUNT, unitPrice, minFillAmount, block.timestamp + 600);
@@ -141,11 +140,18 @@ contract UpgradeInfinityForCMARKCredits is TestHelper, AssertionHelper, Listings
         vm.startPrank(RETIRER);
         // confirm retirement functions after upgrade as well (without revert)
         uint256 cmarkRetireId2 = diamond.retireCarbonmarkListing(
-            listingStruct, SOURCE_AMOUNT, AMOUNT/2, details, LibTransfer.From.EXTERNAL
+            listingStruct, SOURCE_AMOUNT / 2, AMOUNT / 4, details, LibTransfer.From.EXTERNAL
         );
 
+        assertEq(IERC20(CMARK_ADDRESS).balanceOf(RETIRER), AMOUNT/4);
+        assertEq(IERC20(CMARK_ADDRESS).balanceOf(DIAMOND_ADDRESS), AMOUNT/2);
+        assertEq(marketplace.getRemainingAmount(cmarkListingId), AMOUNT/4);
+
+        RetireCMARKFacet rcmDiamond = RetireCMARKFacet(DIAMOND_ADDRESS);
+        uint256 cmarkRetireId3 = rcmDiamond.cmarkRetireExactCarbon(
+            CMARK_ADDRESS, AMOUNT/4, details, LibTransfer.From.EXTERNAL
+        );
         assertEq(IERC20(CMARK_ADDRESS).balanceOf(RETIRER), 0);
-        assertEq(marketplace.getRemainingAmount(cmarkListingId), 0);
         vm.stopPrank();
 
     }
