d1716e70bdd82d55939e285d89fc4d1d1c5727c4
psparacino <tufnelenterprises@gmail.com>
2025-02-27 10:24:10 -0500

    add custom_lib

--- src/infinity/facets/RetirementQuoter.sol
+++ src/infinity/facets/RetirementQuoter.sol
@@ -7,7 +7,7 @@ import "../libraries/TokenSwap/LibSwap.sol";
 import "../C.sol";
 import "../AppStorage.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
-import "@uniswap/v3-periphery/contracts/interfaces/IQuoterView.sol";
+import "../../custom_lib/IQuoterView.sol";
 
 /**
  * @author Cujo
