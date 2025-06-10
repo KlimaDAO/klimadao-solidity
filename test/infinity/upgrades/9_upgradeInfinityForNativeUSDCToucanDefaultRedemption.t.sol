d47bd52d564f0b8e714e2819da59cf8cf3953d43
psparacino <tufnelenterprises@gmail.com>
2025-06-10 10:23:42 -0400

    feat: add deployment scripts + test

---
+++ test/infinity/upgrades/9_upgradeInfinityForNativeUSDCToucanDefaultRedemption.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {UpgradeInfinityForNativeUSDCToucanDefaultRedemption} from "../../../script/9_upgradeInfinityForNativeUSDCToucanDefaultRedemption.sol";
+import {TestHelper} from "../../infinity/TestHelper.sol";
+
+import "../TestHelper.sol";
+
+contract UpgradeInfinityForNativeUSDCToucanDefaultRedemptionTest is TestHelper {
+    function setUp() public {
+        // Start fork
+        string memory RPC_URL = vm.envString("POLYGON_URL");
+        uint256 forkId = vm.createFork(RPC_URL);
+        vm.selectFork(forkId);
+    }
+
+    function test_upgrade() public {
+        address DIAMOND_OWNER = vm.envAddress("INFINITY_OWNER"); // multisig
+        address DIAMOND_ADDRESS = vm.envAddress("INFINITY_ADDRESS");
+
+        // Set the calldata
+        UpgradeInfinityForNativeUSDCToucanDefaultRedemption upgradeScript = new UpgradeInfinityForNativeUSDCToucanDefaultRedemption();
+        console2.log("Upgrade script address");
+        console2.logAddress(address(upgradeScript));
+        bytes memory data = upgradeScript.run();
+
+        // Prank the owner of the Diamond
+        vm.startPrank(DIAMOND_OWNER);
+        (bool success,) = DIAMOND_ADDRESS.call(data);
+        require(success, "Diamond upgrade failed");
+        vm.stopPrank();
+    }
+}
